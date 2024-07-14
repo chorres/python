162e42545d99d30454602bbf153927be2e666735
chorres <chorres.rodriguez.jhonatan@gmail.com>
2024-07-14 01:17:31 -0500

    ejercicio condicional practica

---
+++ practica/conditionalPractica.py
@@ -0,0 +1,51 @@
+#Número Par o Impar
+
+
+# num = int(input("Ingresa un número: "))
+
+# if num == 0:
+#     print(f"{num} es número neutro")
+# elif num % 2 == 0:
+#     print(f"{num} es número par")
+# else:
+#     print(f"{num} es número impar")
+
+# num=int(input("Ingresar un número: "))
+
+# if num==0:
+#     print(f"{num} es neutro")
+# elif num>0:
+#     if num%2==0:
+#         print(f"{num} es positivo y par")
+#     else:
+#         print(f"{num} es positivo e impar")
+# else:
+#     if num%2==0:
+#         print(f"{num} es negativo y par")
+#     else:
+#         print(f"{num} es negativo e impar")
+
+
+# num=int(input("Ingresar 1# número: "))
+# num2=int(input("Ingresar 2# número: "))
+# num3=int(input("Ingresar 3# número: "))
+
+# if num>num2 and num>num3:
+#     if num2>num3:
+#         print(f"{num} - {num2} - {num3}")
+#     else:
+#         print(f"{num} - {num3} - {num2}")
+# elif num2>num and num2>num3:
+#     if num>num3:
+#         print(f"{num2} - {num} - {num3}")
+#     else:
+#         print(f"{num2} - {num3} - {num}")
+# else:
+#     if num>num2:
+#         print(f"{num3} - {num} - {num2}")
+#     else:
+#         print(f"{num3} - {num2} - {num}")
+
+
+
+
